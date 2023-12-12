f4bb7544ba76a92edec6430f7cdf16a36f29ca37
Bokurai <mrnere88@gmail.com>
2023-12-12 12:22:07 +0100

    lefsdpf

--- app/src/main/java/com/example/m08_practicafinaluf1_nereidabarba/HomeFragment.java
+++ app/src/main/java/com/example/m08_practicafinaluf1_nereidabarba/HomeFragment.java
@@ -2,14 +2,29 @@ package com.example.m08_practicafinaluf1_nereidabarba;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.m08_practicafinaluf1_nereidabarba.databinding.FragmentHomeBinding;
+
 
 public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (binding = FragmentHomeBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 
 }
--- app/src/main/java/com/example/m08_practicafinaluf1_nereidabarba/ToDoListFragment.java
+++ app/src/main/java/com/example/m08_practicafinaluf1_nereidabarba/ToDoListFragment.java
@@ -2,14 +2,29 @@ package com.example.m08_practicafinaluf1_nereidabarba;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.m08_practicafinaluf1_nereidabarba.databinding.FragmentToDoListBinding;
+
 
 public class ToDoListFragment extends Fragment {
 
+    private FragmentToDoListBinding binding;
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (binding = FragmentToDoListBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
